f1e382a0f52ce3baafe814f71a35ad09f5aed306
Lyndon Maydwell <lyndon@sordina.net>
2013-08-04 12:10:37 +1000

    Checking for differences

--- resources/scripts/check.sh
+++ resources/scripts/check.sh
@@ -1,6 +1,21 @@
 rm -rf /tmp/haskell_workshop_check.*
 cat > /tmp/haskell_workshop_check.hs
 
+touch /tmp/haskell_workshop_md5_$PANDOCTOR_COUNT
+
+checksum=`md5 /tmp/haskell_workshop_check.hs`
+previous=`cat /tmp/haskell_workshop_md5_$PANDOCTOR_COUNT`
+
+if [[ "$checksum" = "$previous" ]]
+then
+	echo "Skipping $PANDOCTOR_COUNT"
+	exit 0
+fi
+
+echo "Processing $PANDOCTOR_COUNT"
+
+echo $checksum > /tmp/haskell_workshop_md5_$PANDOCTOR_COUNT
+
 grep '^main =' /tmp/haskell_workshop_check.hs
 status=$?
 if [ $status -ne 0 ]
